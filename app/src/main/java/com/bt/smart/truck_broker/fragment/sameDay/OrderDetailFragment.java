3d2d909381f1be19adfb90ad4834311de58bcaae
qpy1992 <794429910@qq.com>
2019-05-29 17:32:16 +0800

    05-19

--- app/src/main/java/com/bt/smart/truck_broker/fragment/sameDay/OrderDetailFragment.java
+++ app/src/main/java/com/bt/smart/truck_broker/fragment/sameDay/OrderDetailFragment.java
@@ -6,20 +6,36 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.IBinder;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.bt.smart.truck_broker.MyApplication;
 import com.bt.smart.truck_broker.NetConfig;
 import com.bt.smart.truck_broker.R;
@@ -41,7 +57,11 @@ import com.bt.smart.truck_broker.utils.ToastUtils;
 import com.google.gson.Gson;
 import com.uuzuche.lib_zxing.activity.CodeUtils;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 
 import okhttp3.Request;
 
@@ -56,23 +76,21 @@ import okhttp3.Request;
 
 public class OrderDetailFragment extends Fragment implements View.OnClickListener {
     private View            mRootView;
-    private ImageView       img_back;
-    private ImageView       img_empty;
-    private TextView        tv_title;
+    private ImageView       img_back,img_empty,iv_l1,iv_l2,iv_l3,iv_load,iv_r1,iv_r2,iv_r3,iv_rece;
+    private LinearLayout    ll_load,ll_rece;
+    private TextView        tv_title,tv_place,tv_goodsname,tv_carType,tv_name,tv_fhPlace,tv_phone,tv_cont,tv_take;
     private String          orderID;//订单id
     private String          mOrder_no;//订单号
-    private TextView        tv_place;
-    private TextView        tv_goodsname;
-    private TextView        tv_carType;
-    private TextView        tv_name;
-    private TextView        tv_fhPlace;
-    private TextView        tv_phone;
     //    private TextView        tv_local;//开始定位按钮
-    private TextView        tv_cont;//联系货主
-    private TextView        tv_take;//接单
     private OrderDetailInfo orderDetailInfo;//订单详情
     private int RESULT_TAKE_ORDER = 12088;//接单成功响应值
     private Handler mProhandler;
+    private Uri imageUri;
+    private static final int TAKE_PHOTO = 1002;
+    private Bitmap bitmap1 = null;
+    // 记录文件保存位置
+    private String mFilePath;
+    private FileInputStream is = null;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -95,6 +113,16 @@ public class OrderDetailFragment extends Fragment implements View.OnClickListene
         tv_cont = mRootView.findViewById(R.id.tv_cont);
         tv_take = mRootView.findViewById(R.id.tv_take);
         //        tv_local = mRootView.findViewById(R.id.tv_local);
+        iv_l1 = mRootView.findViewById(R.id.iv_l1);
+        iv_l2 = mRootView.findViewById(R.id.iv_l2);
+        iv_l3 = mRootView.findViewById(R.id.iv_l3);
+        iv_load = mRootView.findViewById(R.id.iv_load);
+        iv_r1 = mRootView.findViewById(R.id.iv_r1);
+        iv_r2 = mRootView.findViewById(R.id.iv_r2);
+        iv_r3 = mRootView.findViewById(R.id.iv_r3);
+        iv_rece = mRootView.findViewById(R.id.iv_rece);
+        ll_load = mRootView.findViewById(R.id.ll_load);
+        ll_rece = mRootView.findViewById(R.id.ll_rece);
     }
 
     private void initData() {
@@ -107,6 +135,14 @@ public class OrderDetailFragment extends Fragment implements View.OnClickListene
         tv_cont.setOnClickListener(this);
         tv_take.setOnClickListener(this);
         img_empty.setOnClickListener(this);
+        iv_load.setOnClickListener(this);
+        iv_rece.setOnClickListener(this);
+        iv_l1.setOnClickListener(this);
+        iv_l2.setOnClickListener(this);
+        iv_l3.setOnClickListener(this);
+        iv_r1.setOnClickListener(this);
+        iv_r2.setOnClickListener(this);
+        iv_r3.setOnClickListener(this);
         //        tv_local.setOnClickListener(this);
         //        String touchKind = getActivity().getIntent().getStringExtra("touchKind");
         //        if (null != touchKind && "accepted".equals(touchKind)) {
@@ -115,11 +151,17 @@ public class OrderDetailFragment extends Fragment implements View.OnClickListene
         int orderType = getActivity().getIntent().getIntExtra("orderType", -1);
         if (0 == orderType || 1 == orderType || 2 == orderType || 5 == orderType) {
             tv_take.setText("开锁");
+            ll_load.setVisibility(View.VISIBLE);
+            ll_rece.setVisibility(View.VISIBLE);
         } else if (3 == orderType || 4 == orderType || 6 == orderType) {
             tv_take.setVisibility(View.GONE);
         }
         //初始化定时刷新器
         initHandlerPost();
+        // 获取SD卡路径
+        mFilePath = Environment.getExternalStorageDirectory().getPath();
+        // 文件名
+        mFilePath = mFilePath + "/" + "photo.png";
     }
 
     @Override
@@ -151,6 +193,41 @@ public class OrderDetailFragment extends Fragment implements View.OnClickListene
             //                //                ToastUtils.showToast(getContext(), "开始定位...");
             //                //                startSendLanAlat();
             //                break;
+            case R.id.iv_load:
+                //启动相机程序
+                Intent intent=new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                // 加载路径
+                Uri uri;
+                if (Build.VERSION.SDK_INT >= 24) {
+                    uri = FileProvider.getUriForFile(getActivity().getApplicationContext(), "com.bt.smart.truck_broker.fileprovider", new File(mFilePath));
+                } else {
+                    uri = Uri.fromFile(new File(mFilePath));
+                }
+                // 指定存储路径，这样就可以保存原图了
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+                startActivityForResult(intent,TAKE_PHOTO);
+                break;
+            case R.id.iv_rece:
+
+                break;
+            case R.id.iv_l1:
+                expandImg(bitmap1);
+                break;
+            case R.id.iv_l2:
+
+                break;
+            case R.id.iv_l3:
+
+                break;
+            case R.id.iv_r1:
+
+                break;
+            case R.id.iv_r2:
+
+                break;
+            case R.id.iv_r3:
+
+                break;
         }
     }
 
@@ -165,32 +242,62 @@ public class OrderDetailFragment extends Fragment implements View.OnClickListene
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        /**
-         * 处理二维码扫描结果
-         */
-        if (requestCode == REQUEST_CODE) {
-            //处理扫描结果，将扫描获取到的编码上传给服务器
-            if (null != data) {
-                Bundle bundle = data.getExtras();
-                if (bundle == null) {
-                    return;
-                }
-                if (bundle.getInt(CodeUtils.RESULT_TYPE) == CodeUtils.RESULT_SUCCESS) {
-                    String result = bundle.getString(CodeUtils.RESULT_STRING);
-                    mBlueXlh = result;
-                    //上传给服务器,获取mac地址
-                    ToastUtils.showToast(getContext(), "获取设备锁信息");
-                    if (null == mBlueMac) {
-                        sendCode2Service(result);
-                    } else {
-                        //跳转连接蓝牙界面
-                        contactBlueDevice();
+//        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case REQUEST_CODE:
+                //处理扫描结果，将扫描获取到的编码上传给服务器
+                if (null != data) {
+                    Bundle bundle = data.getExtras();
+                    if (bundle == null) {
+                        return;
                     }
-                } else if (bundle.getInt(CodeUtils.RESULT_TYPE) == CodeUtils.RESULT_FAILED) {
-                    Toast.makeText(getContext(), "解析二维码失败", Toast.LENGTH_LONG).show();
+                    if (bundle.getInt(CodeUtils.RESULT_TYPE) == CodeUtils.RESULT_SUCCESS) {
+                        String result = bundle.getString(CodeUtils.RESULT_STRING);
+                        mBlueXlh = result;
+                        //上传给服务器,获取mac地址
+                        ToastUtils.showToast(getContext(), "获取设备锁信息");
+                        if (null == mBlueMac) {
+                            sendCode2Service(result);
+                        } else {
+                            //跳转连接蓝牙界面
+                            contactBlueDevice();
+                        }
+                    } else if (bundle.getInt(CodeUtils.RESULT_TYPE) == CodeUtils.RESULT_FAILED) {
+                        Toast.makeText(getContext(), "解析二维码失败", Toast.LENGTH_LONG).show();
+                    }
                 }
-            }
+                break;
+            case TAKE_PHOTO:
+                    //将拍摄的照片显示出来
+                try {
+                    try {
+                        // 获取输入流
+                        is = new FileInputStream(mFilePath);
+                        // 把流解析成bitmap
+                        bitmap1 = BitmapFactory.decodeStream(is);
+                        //*****旋转一下
+                        Matrix matrix = new Matrix();
+                        matrix.postRotate(90);
+                        bitmap1 = Bitmap.createBitmap(bitmap1, 0, 0, bitmap1.getWidth(), bitmap1.getHeight(), matrix, true);
+                        // 设置图片
+                        iv_l1.setVisibility(View.VISIBLE);
+                        iv_l1.setImageBitmap(bitmap1);
+                    } catch (FileNotFoundException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    } finally {
+                        // 关闭流
+                        try {
+                            is.close();
+                        } catch (IOException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+                    }
+                }catch (NullPointerException e){
+                    e.printStackTrace();
+                }
+                break;
         }
     }
 
@@ -237,8 +344,8 @@ public class OrderDetailFragment extends Fragment implements View.OnClickListene
         getScanCode();
     }
 
-    private int MY_PERMISSIONS_REQUEST_CALL_PHONE2 = 1001;//申请照相机权限结果
-    private int REQUEST_CODE                       = 1003;//接收扫描结果
+    private static final int MY_PERMISSIONS_REQUEST_CALL_PHONE2 = 1001;//申请照相机权限结果
+    private static final int REQUEST_CODE                       = 1003;//接收扫描结果
     private String mBlueMac;
     private String mBlueXlh;
 
@@ -397,6 +504,23 @@ public class OrderDetailFragment extends Fragment implements View.OnClickListene
                     tv_fhPlace.setText(orderDetailInfo.getData().getFh_address());
                     tv_phone.setText(orderDetailInfo.getData().getFh_telephone());
                 }
+            }
+        });
+    }
+
+    protected void expandImg(Bitmap bitmap){
+        ImageView iv = new ImageView(getActivity());
+        iv.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        iv.setImageBitmap(bitmap);
+        final AlertDialog builder = new AlertDialog.Builder(getActivity(), android.R.style.Theme_Black_NoTitleBar_Fullscreen).create();
+        builder.show();
+        Window window = builder.getWindow();
+        window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        window.clearFlags(WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
+        window.setContentView(iv);
+        iv.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View paramView) {
+                builder.cancel();
             }
         });
     }
