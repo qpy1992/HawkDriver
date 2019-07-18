8a4cbd4799b7780476cb40b6e01c406ee2733263
YJQ <admin>
2019-07-18 10:59:58 +0800

    some change

--- app/src/main/java/com/bt/smart/truck_broker/activity/FirstActivity.java
+++ app/src/main/java/com/bt/smart/truck_broker/activity/FirstActivity.java
@@ -126,7 +126,7 @@ public class FirstActivity extends Activity implements View.OnClickListener {
     private static final int REQUEST_PERMISSION = 0;
 
     private void requestPermission() {
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_PHONE_STATE},
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_PHONE_STATE, Manifest.permission.CAMERA},
                 REQUEST_PERMISSION);
     }
 
